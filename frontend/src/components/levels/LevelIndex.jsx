3debcff20932de3b194b1f436421e80e53a228f0
Pedro Pascutti <73670297+PedroPascutti@users.noreply.github.com>
2023-11-06 16:20:04 -0300

    Feat: Added skeleton on component render data (#6)
    Co-authored-by: Pedro <pedro@prestigetecnologia.com.br>

--- frontend/src/components/levels/LevelIndex.jsx
+++ frontend/src/components/levels/LevelIndex.jsx
@@ -9,7 +9,7 @@ import { LevelEdit } from "./LevelEdit";
 import { LevelDelete } from "./LevelDelete";
 
 export const LevelIndex = () => {
-  const { levels, getLevels, getLevel, previousPage, nextPage } = useContext(LevelContext);
+  const { levels, getLevels, getLevel, previousPage, nextPage, isLoading } = useContext(LevelContext);
   const [id, setId] = useState(null);
 
   const [openRegisterModal, setOpenRegisterModal] = useState(false);
@@ -41,6 +41,8 @@ export const LevelIndex = () => {
   const handleNextPageClick = () => {
     nextPage();
   }
+
+  const skeleton = 6;
 
   useEffect(() => {
     getLevels("http://localhost:8000/levels");
@@ -79,47 +81,68 @@ export const LevelIndex = () => {
                 </th>
             </tr>
           </thead>
-          <tbody className="table__body">
-            {levels.map((level) => (
-              <tr key={level.id} className="table__row">
-                <th scope="row" className="table__cell table__cell-main">
-                  {level.name}
-                </th>
+          {isLoading && (
+            <tbody className="table__body animate-pulse" role="status">
+            {[...Array(skeleton)].map((e, i) => (
+              <tr key={i} className="table__row">
                 <td className="table__cell">
-                  {level.developers_amount}
+                  <div class="skeleton__content w-24"></div>
+                </td>
+                <td className="table__cell">
+                  <div class="skeleton__content"></div>
                 </td>
                 <td className="table__cell justify-end flex">
-                    <div className="flex gap-3">
-                        <IconContext.Provider value={{ size: "18px", color: '#7fff7f' }}>
-                            <button value={level.id} onClick={handleEditClick}>
-                                <FaPenToSquare />
-                            </button>
-                        </IconContext.Provider>
-                        <IconContext.Provider value={{ size: "18px", color: '#ff5252' }}>
-                            <button value={level.id} onClick={handleDeleteClick}>
-                                <FaTrash />
-                            </button>
-                        </IconContext.Provider>
-                    </div>
+                  <div class="skeleton__content"></div>
                 </td>
               </tr>
             ))}
-          </tbody>
+            </tbody>
+          )}
+          {!isLoading && (
+            <tbody className="table__body">
+              {levels.map((level) => (
+                <tr key={level.id} className="table__row">
+                  <th scope="row" className="table__cell table__cell-main">
+                    {level.name}
+                  </th>
+                  <td className="table__cell">
+                    {level.developers_amount}
+                  </td>
+                  <td className="table__cell justify-end flex">
+                      <div className="flex gap-3">
+                          <IconContext.Provider value={{ size: "18px", color: '#7fff7f' }}>
+                              <button value={level.id} onClick={handleEditClick}>
+                                  <FaPenToSquare />
+                              </button>
+                          </IconContext.Provider>
+                          <IconContext.Provider value={{ size: "18px", color: '#ff5252' }}>
+                              <button value={level.id} onClick={handleDeleteClick}>
+                                  <FaTrash />
+                              </button>
+                          </IconContext.Provider>
+                      </div>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          )}
         </table>
       </div>
 
-      <nav className="pagination">
-        <ul className="pagination__list">
-          <li onClick={handlePreviousPageClick} className="pagination__list-item rounded-l-lg">
-            <span className="sr-only">Previous</span>
-            <MdNavigateBefore />
-          </li>
-          <li onClick={handleNextPageClick} className="pagination__list-item rounded-r-lg">
-            <span className="sr-only">Next</span>
-            <MdOutlineNavigateNext />
-          </li>
-        </ul>
-      </nav>
+      {!isLoading && (
+        <nav className="pagination">
+          <ul className="pagination__list">
+            <li onClick={handlePreviousPageClick} className="pagination__list-item rounded-l-lg">
+              <span className="sr-only">Previous</span>
+              <MdNavigateBefore />
+            </li>
+            <li onClick={handleNextPageClick} className="pagination__list-item rounded-r-lg">
+              <span className="sr-only">Next</span>
+              <MdOutlineNavigateNext />
+            </li>
+          </ul>
+        </nav>
+      )}
 
       <LevelCreate show={openRegisterModal} onClose={handleRegisterClose} />
       <LevelEdit show={openEditModal} onClose={handleEditClose} />
